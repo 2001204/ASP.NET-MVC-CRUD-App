e3334c357261e4201a799832b983dd57f8e1c228
Dipak Wani <126748063+2001204@users.noreply.github.com>
2024-01-13 10:23:38 +0530

    Add files via upload

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,67 @@
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyMVCDemo1.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DemoRazor() 
+        {
+            //ViewData    : It store into Dictionary key value pair 
+            //ViewBag
+            //here Cdac is key 
+            ViewData["Cdac"] = "CDAC";
+
+            //here Address is key and we can use in cdhtml page by @View.Address and whole address you can store in one key
+            ViewData["Address1"] = new Address { Id = 1, City = "Jalgaon", Streetname = "Bhusawal Road" };
+            
+            
+             ViewBag.Address = new Address()
+            {
+                Id = 1,
+                Streetname = "Bhusawal Road",
+                City = "Jalgaon"
+            };
+            return View();
+        }
+        public IActionResult ShowDetails()
+        {
+            ViewBag.FirstName = "Dipak";
+            ViewBag.LastName = "Wani";
+            ViewBag.Add1 = new Address { Id = 101, City = "Mumbai", Streetname = "Jalgaon Road", Country = "India"  };
+            return View(); 
+        }
+
+        public IActionResult PartialDemo()
+        {
+            return View();
+        }
+
+        public IActionResult MyMethod()   //not work
+        {
+            return View();
+        }
+
+        public IActionResult MyPartiaView() 
+        {
+            return PartialDemo();
+        }
+    }
+
+    internal class Address
+    {
+        public Address()
+        {
+
+        }
+        public int Id { get; set; }
+        public string Streetname { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ Data/MyWebAppDb.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyMVCDemo1.Models;
+
+namespace MyMVCDemo1.Data
+{
+    public class MyWebAppDb: DbContext
+    {
+        //it have one constructor 
+        public MyWebAppDb(DbContextOptions options):base(options) 
+        {
+
+        }
+
+        //list o tables to be operated by DbContext
+        //it will create Employees table for Employee model class
+        public DbSet<Employee> Employees { get; set; }
+
+        //it will create Depts table for Employee model class
+        public DbSet<Dept> Depts { get; set; }
+
+        //it will create Persons table for Employee model class
+        public DbSet<Person> Persons { get; set; }
+
+
+        //join table queries are DTOs are also registerd here 
+
+
+
+        //ModalBuilder : 
+
+    }
+}
