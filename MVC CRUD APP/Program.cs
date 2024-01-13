1ab11a1261957e58febc9782171ddedc4012838e
Dipak Wani <126748063+2001204@users.noreply.github.com>
2024-01-13 10:38:28 +0530

    Add files via upload

---
+++ MVC CRUD APP/Program.cs
@@ -0,0 +1,53 @@
+using Microsoft.EntityFrameworkCore;
+using MyMVCDemo1.Data;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace MyMVCDemo1
+{
+    public class Program
+    {
+        private static object services;
+
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            //connection setting
+            builder.Services.AddDbContext<MyWebAppDb>(options =>
+            {
+                var connectionString =
+               builder.Configuration.GetConnectionString("DefaultConnection");
+                options.UseMySql(connectionString,
+               ServerVersion.AutoDetect(connectionString));
+            });
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
